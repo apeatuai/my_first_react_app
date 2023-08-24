6e2e32a103e684b8d4d4bfcf3ee9dd3d98fc1584
Gregory <gregurim12@gmail.com>
2023-08-24 10:21:14 +0000

    first-react_app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+
+          <SkillList skill="Angular,HTML,CSS,Javacript" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// greg
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://img.freepik.com/free-photo/fashion-boy-with-yellow-jacket-blue-pants_71767-96.jpg?size=626&ext=jpg"
+      alt="avats"
+    />
+  );
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Nana Greg</h1>
+      <p>
+        In the fast-paced world of technology, stagnation is the enemy of
+        progress. Greg understands this well and is a lifelong learner. He stays
+        up-to-date with the latest programming languages, frameworks, and best
+        practices. Greg's commitment to continuous improvement not only keeps
+        him at the forefront of his field but also elevates the projects he's
+        involved in.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="🙄" />
+      <Skill skill="Angular" emoji="🥰" />
+      <Skill skill="HTML + CSS" emoji="😉" />
+      <Skill skill="TypeScript" emoji="😎" />
+      <Skill skill="Googling" emoji="😙" />
+      <Skill skill="Java" emoji="😁" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ color: "white" }}>
+      <span>{props.skill} </span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
